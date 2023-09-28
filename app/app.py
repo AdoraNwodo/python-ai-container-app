d1478b373856b0e3dc889351519560bad5975c32
Adora Nwodo <adoranwodo@Adoras-MacBook-Pro.local>
2023-09-28 20:05:41 +0100

    first commit

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from langchain.schema import (
+    HumanMessage,
+    SystemMessage
+)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return { "message": "Hello, world forever!!!" }
